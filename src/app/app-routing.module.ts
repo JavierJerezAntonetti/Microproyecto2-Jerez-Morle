26323139f08f13495875c9e1bda95ce2a24481c8
Yoselyn Morle <m.yoselyn@correo.unimet.edu.ve>
2021-03-05 17:14:05 -0400

    Style
    Agregado nuevo diseño e implementado el componente en la vista home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import {ListapeliculasComponent} from './components/listapeliculas/listapeliculas.component'
 
 const routes: Routes = [
 
   {path: '', component: HomeComponent},
+  {path: '', component: ListapeliculasComponent}
 
 ];
 
