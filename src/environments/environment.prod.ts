c2411b024a64dbf73feac54f7f286d1216ccd880
Yoselyn Morle <m.yoselyn@correo.unimet.edu.ve>
2021-03-05 09:52:21 -0400

    Firebase
    Instalado y configurado Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAYQR4jAUBqqzggJbk_qFFBupASHEodGW8",
+    authDomain: "microproyecto2-jerez-morle.firebaseapp.com",
+    projectId: "microproyecto2-jerez-morle",
+    storageBucket: "microproyecto2-jerez-morle.appspot.com",
+    messagingSenderId: "1051376663824",
+    appId: "1:1051376663824:web:264a1d8ecf24d43866b995",
+    measurementId: "G-92NV5L103N"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: "AIzaSyAYQR4jAUBqqzggJbk_qFFBupASHEodGW8",
+    authDomain: "microproyecto2-jerez-morle.firebaseapp.com",
+    projectId: "microproyecto2-jerez-morle",
+    storageBucket: "microproyecto2-jerez-morle.appspot.com",
+    messagingSenderId: "1051376663824",
+    appId: "1:1051376663824:web:264a1d8ecf24d43866b995",
+    measurementId: "G-92NV5L103N"
+  },
 };
 
 /*
